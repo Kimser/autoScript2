b5abd29d1b26fe0d8fc7d49d43385b81801fdb06
Kimser <kimser@126.com>
2024-03-01 17:11:33 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import json from '@rollup/plugin-json'
 export default {
     input: 'src/index.js',
     output: {
-        file: './dist/index.js',
+        file: 'dist/index.js',
         format: 'es',
         sourcemap: true,
     },
